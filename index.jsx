93e1e4d1c38969c77f0a250580ed87356ce46ad8
Lein0rb4gX <lein0rb4g@gmail.com>
2024-02-13 07:41:43 -0300

    first commit

---
+++ index.jsx
@@ -0,0 +1,15 @@
+const element = <h1>Olá mundo!</h1>
+const htmlObject = (
+    <div>
+        <h1>
+            Título
+        </h1>
+
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+)
